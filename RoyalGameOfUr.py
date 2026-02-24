1b6a4603458f94041cae9353f3c63e5fb0f11669
Joe deBlois <jdeblois@uiowa.edu>
2026-02-24 09:37:31 -0600

    Finished full pipeline. Need to 1) fix erros in turn-taking, 2) modularization of turn() for determining possible moves

--- RoyalGameOfUr.py
+++ RoyalGameOfUr.py
@@ -9,7 +9,7 @@ starting_zonep1 = [0, 0, 0, 0] #idx 3 is rosette, space 4
 starting_zonep2 = [0, 0, 0, 0] #idx 3 is rosette, space 4
 combat_zone = [0, 0, 0, 0, 0, 0, 0, 0] #idx 3 is rosette, space 4
 home_zonep1 = [0, 0, 10]    #idx 1 is rosette (space 2), idx 2 scores (value 10)
-home_zonep2 = [0, 0, 10]    #idx 1 is rosette (space 2), idx 2 scores (value 10)
+home_zonep2 = [0, 0, 10, 3, 3, 3, 3]    #idx 1 is rosette (space 2), idx 2 scores (value 10)
 
 #pieces & score
 dark = "dark"
@@ -26,18 +26,21 @@ def calibrate_turn_variables(current_player):
     print(f"\ncalibrating turn parameters for player {current_player}\n")
     
     if current_player == 1:
-        turn(starting_zonep1, home_zonep1, p1_pieces, p2_pieces, p1_score, dark, 2, 1)
+        return turn(starting_zonep1, home_zonep1, p1_pieces, p2_pieces, p1_score, dark, light, 2, 1)
     else:
-        turn(starting_zonep2, home_zonep2, p2_pieces, p1_pieces, p2_score, light, 1, 2)
+        return turn(starting_zonep2, home_zonep2, p2_pieces, p1_pieces, p2_score, light, dark, 1, 2)
 
              
 #all possible moves and steps for a turn
-def turn(starting_zone, home_zone, player_pieces, opponent_pieces, score, color, next_player, current_player):
+def turn(starting_zone, home_zone, player_pieces, opponent_pieces, score, color, opp_color, next_player, current_player):
 
     print(f"starting turn for player {current_player}")
 
     #1) roll die: random choice between 0 and 4
-    roll = random.randint(0, 4)
+    roll = random.choices(
+        population = [0, 1, 2, 3, 4], 
+        k = 1, 
+        weights = [(1/16), (4/16), (6/16), (4/16), (1/16)] )[0]
     print(f"Roll: {roll}")
 
     #if no pieces on board for current player, place one
@@ -48,75 +51,283 @@ def turn(starting_zone, home_zone, player_pieces, opponent_pieces, score, color,
         print(f"Player {current_player} pieces: {player_pieces}")
         if  (roll) == 4: #if landed on rosette, go again
             print(f"Landed on rosette. Player {current_player} goes again.")
-            turn(starting_zone, home_zone, player_pieces, opponent_pieces, score, color, next_player, current_player)
+            return {"score": score, "next_player": current_player}
         else: #else, on to next player's turn
-            post_move(current_player, next_player, score)
+            return {"score" : score, "next_player":next_player}
     
     # for all pieces that can move, ask for user input on which to move
     # write moves first, then designate them to their own fucntions that the user can choose from.
     # if opponent is on rosetta, they're safe 
     #determine legal possible moves
-    #list of start space, start desc, end space, end desc, capture (T/F), land on rosette (T/F)
+    #list of dictionaries.
+    #keys are "from_zone", "from_idx", "to_zone", "to_idx", "capture", "rosette", "score", "from_desc", "to_desc"
     possible_moves = []
 
-    print("determining possible moves")
+    print("Determining possible moves...")
 
     ##### movement & capture of pieces already on board #####
-    #indices for pieces in starting zone (if any)
-    #list of positions
+   
     if roll != 0:
+        ###starting zone###
         sz_pieces = [(idx + 1) for idx, space in enumerate(starting_zone) if space == color]
-        for idx in sz_pieces: 
+        for space in sz_pieces: 
             #if movement would go into combat zone
-            if (idx + roll) > len(starting_zone):
+            if (space + roll) > len(starting_zone):
                 #if the player does not have a piece there
-                if combat_zone[idx + roll - len(starting_zone) -1] != color:
+                if combat_zone[space + roll - len(starting_zone) -1] != color:
                     #if enemy has a piece there
-                    if combat_zone[idx + roll - len(starting_zone) -1] != 0:
-                        possible_moves.append([starting_zone[idx - 1], f"Starting zone space {idx}", combat_zone[idx + roll - len(starting_zone) -1], f"Combat zone space {idx + roll - len(starting_zone)}", True, False])
+                    if combat_zone[space + roll - len(starting_zone) -1] != 0:
+                        possible_moves.append(
+                            {"from_zone" : "starting",
+                            "from_idx" : space - 1, 
+                            "to_zone" : "combat", 
+                            "to_idx" : space + roll - len(starting_zone) -1, 
+                            "capture": True, 
+                            "rosette": False, 
+                            "from_desc": f"starting zone space {space}", 
+                            "to_desc": f"combat zone space {space + roll - len(starting_zone)}"})
                     #if enemy does not have piece there
                     else: 
                         #if rosette
-                        if (idx + roll - len(starting_zone) -1) == 3: 
-                            possible_moves.append([starting_zone[idx - 1], f"Starting zone space {idx}", combat_zone[idx + roll - len(starting_zone) -1], f"Combat zone space {idx + roll - len(starting_zone)}", False, True])
+                        if (space + roll - len(starting_zone)) == 4: 
+                            possible_moves.append(
+                             {"from_zone" : "starting",
+                            "from_idx" : space - 1, 
+                            "to_zone" : "combat", 
+                            "to_idx" : space + roll - len(starting_zone) -1, 
+                            "capture": False, 
+                            "rosette": True, 
+                            "from_desc": f"starting zone space {space}", 
+                            "to_desc": f"combat zone space {space + roll - len(starting_zone)}"})
                         #if not rosette
                         else: 
-                            possible_moves.append([starting_zone[idx - 1], f"Starting zone space {idx}", combat_zone[idx + roll - len(starting_zone) -1], f"Combat zone space {idx + roll - len(starting_zone)}", False, False])
+                            possible_moves.append(
+                            {"from_zone" : "starting",
+                            "from_idx" : space - 1, 
+                            "to_zone" : "combat", 
+                            "to_idx" : space + roll - len(starting_zone) -1, 
+                            "capture": False, 
+                            "rosette": False, 
+                            "from_desc": f"starting zone space {space}", 
+                            "to_desc": f"combat zone space {space + roll - len(starting_zone)}"})
             #if movement would stay in starting zone
             else:
                 #if not a piece there already
-                if starting_zone[idx + roll] == 0:
+                if starting_zone[space + roll - 1] == 0:
                     #if space is rosette: 
-                    if (idx + roll) == 4:
-                        possible_moves.append([starting_zone[idx - 1], f"Starting zone space {idx}", starting_zone[idx + roll - 1], f"Starting zone space {idx + roll}", False, True])
+                    if (space + roll) == 4:
+                        possible_moves.append(
+                            {"from_zone" : "starting",
+                            "from_idx" : space - 1, 
+                            "to_zone" : "starting", 
+                            "to_idx" : space + roll - 1, 
+                            "capture": False, 
+                            "rosette": True, 
+                            "from_desc": f"starting zone space {space}", 
+                            "to_desc": f"starting zone space {space + roll}"})
                     #if space is not rosette
                     else:
-                        possible_moves.append([starting_zone[idx - 1],f"Starting zone space {idx}", starting_zone[idx + roll - 1],f"Starting zone space {idx + roll}", False, False])
-    
+                        possible_moves.append(
+                            {"from_zone" : "starting",
+                            "from_idx" : space - 1, 
+                            "to_zone" : "starting", 
+                            "to_idx" : space + roll - 1, 
+                            "capture": False, 
+                            "rosette": False, 
+                            "from_desc": f"starting zone space {space}", 
+                            "to_desc": f"starting zone space {space + roll}"})
+        
+        ###combat zone###
+        cz_pieces = [(idx + 1) for idx, space in enumerate(combat_zone) if space == color]
+        for space in cz_pieces: 
+            #if movement would go into home zone
+            if (space + roll) > len(combat_zone):
+                #if the player does not have a piece there
+                if home_zone[space + roll - len(combat_zone) -1] != color:
+                    #if score
+                    if (space + roll - len(combat_zone)) == 10: 
+                            possible_moves.append(
+                            {"from_zone" : "combat",
+                            "from_idx" : space - 1, 
+                            "to_zone" : "score", 
+                            "to_idx" : "score", 
+                            "capture": False, 
+                            "rosette": False, 
+                            "from_desc": f"combat zone space {space}", 
+                            "to_desc": "score"})
+                    else: 
+                        if (space + roll - len(combat_zone)) == 2:
+                            possible_moves.append(
+                            {"from_zone" : "combat",
+                            "from_idx" : space - 1, 
+                            "to_zone" : "home", 
+                            "to_idx" : 1, 
+                            "capture": False, 
+                            "rosette": True, 
+                            "from_desc": f"combat zone space {space}", 
+                            "to_desc": "home zone space 2"})
+                        else: 
+                             possible_moves.append(
+                            {"from_zone" : "combat",
+                            "from_idx" : space - 1, 
+                            "to_zone" : "home", 
+                            "to_idx" : 0, 
+                            "capture": False, 
+                            "rosette": False, 
+                            "from_desc": f"combat zone space {space}", 
+                            "to_desc": "home zone space 1"})
 
-    #####introduce a new piece#####
+            #if movement would stay in combat zone
+            else:
+                #if not a piece there already
+                if combat_zone[space + roll -1] == 0:
+                    #if space is rosette: 
+                    if (space + roll) == 4:
+                        possible_moves.append(
+                            {"from_zone" : "combat",
+                            "from_idx" : space - 1, 
+                            "to_zone" : "combat", 
+                            "to_idx" : space + roll - 1, 
+                            "capture": False, 
+                            "rosette": True, 
+                            "from_desc": f"combat zone space {space}", 
+                            "to_desc": f"home zone space {space + roll}"})
+                    #if space is not rosette
+                    else:
+                        possible_moves.append(
+                            {"from_zone" : "combat",
+                            "from_idx" : space - 1, 
+                            "to_zone" : "combat", 
+                            "to_idx" : space + roll - 1, 
+                            "capture": False, 
+                            "rosette": False, 
+                            "from_desc": f"combat zone space {space}", 
+                            "to_desc": f"home zone space {space + roll}"})
+    
+        ###home zone###
+        hz_pieces = [(idx + 1) for idx, space in enumerate(home_zone) if space == color]
+        for space in hz_pieces: 
+            #if piece can score
+            if home_zone[space + roll] == 10:
+                possible_moves.append(
+                            {"from_zone" : "home",
+                            "from_idx" : space - 1, 
+                            "to_zone" : "score", 
+                            "to_idx" : "score", 
+                            "capture": False, 
+                            "rosette": False, 
+                            "from_desc": f"home zone space {space}", 
+                            "to_desc": "score"})
+            else: 
+                #if already in home, only other open space must be rosette
+                if home_zone[space + roll - 1] == 0: 
+                    possible_moves.append(
+                            {"from_zone" : "home",
+                            "from_idx" : space - 1, 
+                            "to_zone" : "home", 
+                            "to_idx" : space + roll - 1, 
+                            "capture": False, 
+                            "rosette": True, 
+                            "from_desc": f"home zone space {space}", 
+                            "to_desc": f"home zone space {space + roll}"})
+
+        #####introduce a new piece#####
+        if starting_zone[roll - 1] == 0: 
+            #if space is free
+            if starting_zone[roll - 1] == 0: 
+                #if rosette
+                if roll == 4: 
+                    possible_moves.append(
+                            {"from_zone" : "place piece",
+                            "from_idx" : "place piece", 
+                            "to_zone" : "starting", 
+                            "to_idx" : roll - 1, 
+                            "capture": False, 
+                            "rosette": True, 
+                            "from_desc": "place piece", 
+                            "to_desc": "starting zone space {roll}"})
+
+                else: 
+                    possible_moves.append(
+                            {"from_zone" : "place piece",
+                            "from_idx" : "place piece", 
+                            "to_zone" : "starting", 
+                            "to_idx" : roll - 1, 
+                            "capture": False, 
+                            "rosette": False, 
+                            "from_desc": "place piece", 
+                            "to_desc": f"starting zone space {roll}"})
 
     #####list moves #####
     print("displaying possible moves...")
+    counter = 0
     if len(possible_moves) == 0: 
         print("No possible moves; skip this turn.")
+        return post_move(current_player, next_player, score)
     else: 
         print("Possible moves: ")
         for move in possible_moves: 
-            print(f"{move[1]} to {move[3]}")
+            print(f"{counter}\t{move['from_desc']} to {move['to_desc']}")
+            counter += 1
+
+    #####player chooses move#####
+    user_input = input("What is your choice?")
+    player_choice = possible_moves[int(user_input)]
+
+    #calibrate variables from player choice#
+
+    from_zone = get_zone(player_choice["from_zone"], starting_zone, home_zone, combat_zone)
+    to_zone = get_zone(player_choice["to_zone"], starting_zone, home_zone, combat_zone)
+
+    from_index = player_choice["from_idx"]
+    to_index = player_choice["to_idx"]
+
+    #make player move#
+
+    #1. score
+    if player_choice["to_idx"] == "score":
+        score +=1
+    #2. move
+    if player_choice["to_idx"] != "score":
+        to_zone[to_index] = color
+
+    #3. "delete" old piece
+    if player_choice["from_zone"] == "place piece":
+        #select a piece to enter the board
+        piece_idx = next((p for p, q in enumerate(player_pieces) if p != 0), None)
+        player_pieces[piece_idx] = 0
+        
+    if player_choice["from_zone"] != "place piece":
+        from_zone[from_index] = 0
+    
+    #4. capture
+    if player_choice["capture"] == True:
+        combat_zone[to_index] = color
+        #choose an opponent's piece slot to be refilled
+        captured_index = next((i for i, v in enumerate(opponent_pieces) if v == 0), None)
+        if captured_index is not None: 
+            opponent_pieces[captured_index] = opp_color
+
+    #4. rosette 
+    if player_choice["rosette"] == True: 
+        print(f"Landed on rosette. Player {current_player} goes again.")
+        #player goes again
+        return ({"score": score, "next_player": current_player})
 
 
-    #####scoring conditions#####
-    #if in combat_zone
-    if combat_zone[-1] == color and roll == 4:
-        score += 1
-        combat_zone[-1] = 0
-    #if in home_zone
-    for space in home_zone:
-        if space == color and home_zone[space + (roll-1)] == 10: 
-            score += 1
-            home_zone[space] = 0
-
+def get_zone(zone_name, starting_zone, home_zone, combat_zone): 
+    if zone_name == "starting":
+        return starting_zone
+    if zone_name == "combat":
+        return combat_zone
+    if zone_name == "home":
+        return home_zone
+    if zone_name == "score": 
+        return "score"
+    if zone_name == "place piece":
+        return "place piece"
+  
+    
 def post_move(current_player, next_player, score):
     #win-check
     if score == 7:
@@ -163,14 +374,24 @@ def post_move(current_player, next_player, score):
         print(" ".join(row))
 
     #finally, change current player
-    current_player = next_player
-    print(f"Turn concluded. Next player is {current_player}")
-    calibrate_turn_variables(current_player)
+    print(f"Turn concluded. Next player is {next_player}")
+    return ({"score": score, "next_player": next_player})
    
 
 #initialize game: choose who rolls first
 current_player = random.randint(1, 2)
 print(f"Player {current_player} goes first")
-calibrate_turn_variables(current_player)
+while p1_score < 7 and p2_score < 7:
+    result = calibrate_turn_variables(current_player)
+    
+    # Update scores
+    if result is not None:
+        if current_player == 1:
+            p1_score = result["score"]
+        else:
+            p2_score = result["score"]
+
+    # Determine next player
+    current_player = result["next_player"]
 
 
